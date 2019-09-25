b8dc8bd9371f128456cf82e0ae842912a85eeb76
Evangelos_Kolimitras <evangeloskolimitras@outlook.com>
2019-09-25 17:33:53 +0200

    expressjs init()

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const express = require('express')
+const app  = express()
+
+
+app.get('/', (req,res,next) => {
+    res.status(200).send('Responsed!')
+})
+
+// Port listening 3000
+app.listen(3000, () => console.log('Listening on port 3000'))
