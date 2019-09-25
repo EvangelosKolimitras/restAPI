9ccc3f3f2e43c9311e8ff3363247fe24d2f5b1fa
Evangelos_Kolimitras <evangeloskolimitras@outlook.com>
2019-09-25 18:01:09 +0200

    mongodb init() and connected

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 
 const express = require('express')
 const app  = express()
+const mongoose = require('mongoose')
+
+const port = process.env.HOSTING_PORT || 3000
+
+// MongoDB init()
+require('dotenv/config')
+const db_connection_string = process.env.DB_CONNECTION_STRING;
+db_connection_string
+    .replace('<username>', process.env.DB_CONNECTION_USERNAME)
+    .replace('<password>', process.env.DB_CONNECTION_PASSWORD)
+
+const db_deprecation_object = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// Connect to databe
+mongoose.connect( db_connection_string, db_deprecation_object, ()=>console.log('Connected to db'))
 
 
 app.get('/', (req,res,next) => {
@@ -8,4 +26,4 @@ app.get('/', (req,res,next) => {
 })
 
 // Port listening 3000
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(port, (port) => console.log(`Listening on port: ${port}`))
