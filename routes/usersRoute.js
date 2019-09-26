2fa118eedf8cdc7be9eb2d59a0aaf6a5b1509649
Evangelos Kolimitras <evangeloskolimitras@gmail.com>
2019-09-26 10:52:52 -0700

    CRUD operations added

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,6 +2,7 @@ const exprees = require('express')
 const router = exprees.Router()
 const User = require('../models/User')
 
+// Basic route
 router.get('/', (req, res, next) => {
     res.json({
         status: 200,
@@ -10,34 +11,100 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.get('/user', (req, res, next) => res.json({
-        status: 200,
-        message: 'success',
-        url : req.url,
-        data : req.query
-    })
-)
-
-router.post('/user/new', (req, res, next) => {
-    const user = new User({
-        name : req.body.name,
-        age : req.body.age,
-        date : req.body.date
-    })
-    user.save().then(user => {
+// Get all users 
+router.get('/users', async (req, res, next) => {
+    try{
+        const usersReturned = await User.find()
         res.json({
             status: 200,
             message: 'success',
             url : req.url,
-            user
+            length: usersReturned.length,
+            usersReturned
         })
-    }).catch(error => res.json({
-        status: 200,
-        message: 'failed',
-        url : req.url,
-        errmsg: error.errmsg
-    }) )
+    }catch(error) {
+        res.status(200).json({
+            message: 'failed',
+            url : req.url,
+            errmsg: error.errmsg
+        })
+    }
+})
 
+// Get a specific user
+router.get('/users/:userId', async (req, res, next) => {
+    try{
+        const usersReturned = await User.findById(req.params.userId)
+        res.json({
+            status: 200,
+            message: 'success',
+            url : req.url,
+            usersReturned
+        })
+    }catch(error) {
+        res.status(200).json({
+            message: 'failed',
+            url : req.url,
+            errmsg: error.errmsg
+        })
+    }
+})
+
+// Delete a specific user
+router.delete('/users/:userId', async (req, res, next) => {
+    try{
+        const usersReturned = await User.findByIdAndDelete(eq.params.userId)
+        res.json({
+            status: 200,
+            message: 'success',
+            url : req.url,
+            usersReturned
+        })
+        console.log(`${usersReturned.name}`)
+    }catch(error) {
+        res.status(200).json({
+            message: 'failed',
+            url : req.url,
+            errmsg: error.errmsg
+        })
+    }
+})
+
+// Update a specific user
+router.patch('/users/:userId', async (req, res, next) => {
+    try{
+        const usersReturned = await User.updateOne(eq.params.userId)
+        res.json({
+            status: 200,
+            message: 'success',
+            url : req.url,
+            usersReturned
+        })
+    }catch(error) {
+        res.status(200).json({
+            message: 'failed',
+            url : req.url,
+            errmsg: error.errmsg
+        })
+    }
+})
+
+router.post('/new-user', async (req, res, next) => {
+    try {
+        const user = new User({
+            name : req.body.name,
+            age : req.body.age,
+            date : req.body.date
+        })
+        const newUser = await user.save();
+        res.json(newUser)
+    } catch (error) {
+        res.status(200).json({
+            message: 'failed',
+            url : req.url,
+            errmsg: error.errmsg
+        })
+    }
 })
 
 module.exports = router
